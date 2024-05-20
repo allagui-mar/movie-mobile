2c0b90c63a02409a9885e219310d24e66ba8ab70
allagui-mar <144329749+allagui-mar@users.noreply.github.com>
2024-05-20 23:46:32 +0100

    Update FavouritesScreen.js

--- screens/FavouritesScreen.js
+++ screens/FavouritesScreen.js
@@ -1,53 +1,44 @@
 
 import  { useState, useEffect } from 'react';
-import { View,  StyleSheet,FlatList } from 'react-native';
+import { View,  StyleSheet,FlatList ,Text} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { useFavorites } from '../store/FavoriteContext';
 import FavoriteItem from '../components/favorite/FavoriteItem';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import LoadingFavorite from '../components/favorite/LoadingFavorite';
+import { Colors } from '../Constants/Colors';
+import Butn from '../components/Butn';
 
 const FavouritesScreen = () => {
   const navigation = useNavigation();
-  const { favorites,setFavorites, removeFavorite } = useFavorites();
+  const { favorites, removeFavorite, loadFavorites } = useFavorites();
   const [loading, setLoading] = useState(true); 
 
-  const loadFavorites = async () => {
-    try {
-      const storedFavorites = await AsyncStorage.getItem('favorites');
-      if (storedFavorites!== null) {
-        const parsedFavorites = JSON.parse(storedFavorites);
-        if (parsedFavorites.length > 0) {
-        
-          setFavorites(parsedFavorites);
-        }
-      }
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setLoading(false);
-    }
-  };
+ 
   useEffect(() => {
-   
-    loadFavorites();
+    const loadData = async () => {
+      await loadFavorites();
+      setLoading(false);
+    }; 
+    loadData();
   }, []);
-
-  if (loading) {
-    return <LoadingFavorite/>
    
-  }
+  if (loading) {
+    return <LoadingFavorite />;
+  };
+  
 
   const renderFavoritesItems = ({ item }) => (
     <FavoriteItem
       item={item}
-      onPress={() => navigation.navigate('Details', { movie: item })}
+      onPress={() => navigation.navigate('Details', { movieId: item.id})}
       removeFavorite={removeFavorite}
     />
   );
 
   return (
     <View style={styles.container}>
+      <Text style={styles.fav}>Favourites</Text>
       <FlatList
         data={favorites}
         numColumns={2}
@@ -55,12 +46,18 @@ const FavouritesScreen = () => {
         renderItem={renderFavoritesItems}
         keyExtractor={(item) => item.id}
       />
+        <Butn style={styles.btnfleche}
+        antIconName='arrowleft'
+        color='#fff'
+        onPress={() => navigation.navigate('Home')}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   flat: {
+    
     justifyContent: 'space-between',
     marginBottom: 15, 
     margin: 10,
@@ -68,11 +65,33 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 50,
-  
+    zIndex: 1,
+  },
+  fav:{
+    fontSize:22,
+    textAlign:'center',
+    paddingBottom:20,
+    fontWeight:'500'
+
+  },
+  btnfleche: {
+    position: 'absolute',
+    top: 45,
+    left: 25,
+    padding: 8,
+   backgroundColor:Colors.Light
   },
 });
 
 export default FavouritesScreen;
+
+
+
+
+
+
+
+
 
 
 
